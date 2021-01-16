fc140931e07e83eb084f650da4a8ec2f5b397e48
Alexander B-R <61094679+alexanderbira@users.noreply.github.com>
2021-01-16 19:51:39 +0000

    create header page

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 
+import profilePic from "../profile.png";
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -9,7 +11,12 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>setup</div>
+      <header>
+        <div id="aboutMe">
+          <img id="profilePic" src={profilePic} />
+          <p id="meDescription">Hey, I'm Alexander and I do functional web design in my free time<br />I'm fairly competent in HTML, CSS, JS (ES6), React, React-Native, PHP, and Python</p>
+        </div>
+      </header>
     );
   }
 }
